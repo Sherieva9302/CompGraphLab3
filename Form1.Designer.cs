1c1c5b299da1d05ab56a4d0e9cabe9aaf6dd0bfa
Sherieva9302 <58978082+Sherieva9302@users.noreply.github.com>
2022-05-27 04:06:25 +0300

    lab3 done

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace lab2_b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(875, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(945, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 54);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите координаты\r\nточек";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1009, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(40, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(1014, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 27);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "x";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1070, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(1076, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 27);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "y";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1009, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(40, 22);
+            this.textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1070, 154);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(40, 22);
+            this.textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1009, 193);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(40, 22);
+            this.textBox5.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1070, 193);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(40, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(1009, 231);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(40, 22);
+            this.textBox7.TabIndex = 21;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1070, 231);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(40, 22);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(1009, 268);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(40, 22);
+            this.textBox9.TabIndex = 23;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(1070, 268);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(40, 22);
+            this.textBox10.TabIndex = 22;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(999, 307);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 36);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(999, 367);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 46);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Построить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 543);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2_b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //построение кривой Безье второго порядка
+        public void curve3(Graphics graphic, Pen pen)
+        {
+            float x, y, x_, y_;
+            int x1 = int.Parse(textBox1.Text);
+            int y1 = int.Parse(textBox2.Text);
+            int x2 = int.Parse(textBox3.Text);
+            int y2 = int.Parse(textBox4.Text);
+            int x3 = int.Parse(textBox5.Text);
+            int y3 = int.Parse(textBox6.Text);
+            x = x1;
+            y = y1;
+            for (float t = 0; t < 1; t += 0.0005f)
+            {
+                x_ = (1 - t) * (1 - t) * x1 + 2 * t * (1 - t) * x2 + t * t * x3;
+                y_ = (1 - t) * (1 - t) * y1 + 2 * t * (1 - t) * y2 + t * t * y3;
+                graphic.DrawLine(pen, x, y, x_, y_);
+                x = x_;
+                y = y_;
+            }
+        }
+
+        //построение кривой Безье третьего порядка
+        public void curve4(Graphics graphic, Pen pen)
+        {
+            float x, y, x_, y_;
+            int x1 = int.Parse(textBox1.Text);
+            int y1 = int.Parse(textBox2.Text);
+            int x2 = int.Parse(textBox3.Text);
+            int y2 = int.Parse(textBox4.Text);
+            int x3 = int.Parse(textBox5.Text);
+            int y3 = int.Parse(textBox6.Text);
+            int x4 = int.Parse(textBox7.Text);
+            int y4 = int.Parse(textBox8.Text);
+            x = x1;
+            y = y1;
+            for (float t = 0; t < 1; t += 0.0005f)
+            {
+                x_ = (1 - t) * (1 - t) * (1 - t) * x1 + 3 * t * (1 - t) * (1 - t) * x2 +
+                3 * t * t * (1 - t) * x3 + t * t * t * x4;
+                y_ = (1 - t) * (1 - t) * (1 - t) * y1 + 3 * t * (1 - t) * (1 - t) * y2 +
+                3 * t * t * (1 - t) * y3 + t * t * t * y4; ;
+                graphic.DrawLine(pen, x, y, x_, y_);
+                x = x_;
+                y = y_;
+            }
+        }
+
+        //построение кривой Безье четвертого порядка
+        public void curve5(Graphics graphic, Pen pen)
+        {
+            float x, y, x_, y_;
+            int x1 = int.Parse(textBox1.Text);
+            int y1 = int.Parse(textBox2.Text);
+            int x2 = int.Parse(textBox3.Text);
+            int y2 = int.Parse(textBox4.Text);
+            int x3 = int.Parse(textBox5.Text);
+            int y3 = int.Parse(textBox6.Text);
+            int x4 = int.Parse(textBox7.Text);
+            int y4 = int.Parse(textBox8.Text);
+            int x5 = int.Parse(textBox9.Text);
+            int y5 = int.Parse(textBox10.Text);
+            x = x1;
+            y = y1;
+
+            for (float t = 0; t < 1; t += 0.0005f)
+            {
+               x_ = (1 - t) * (1 - t) * (1 - t) * (1 - t) * x1 + 4 * t * (1 - t) * (1 -
+               t) * (1 - t) * x2
+                + 6 * t * t * (1 - t) * (1 - t) * x3 + 4 * t * t * t * (1 - t) * x4 +
+               t * t * t * t * x5;
+               y_ = (1 - t) * (1 - t) * (1 - t) * (1 - t) * y1 + 4 * t * (1 - t) * (1 -
+               t) * (1 - t) * y2 + 6 * t * t * (1 - t) * (1 - t) * y3 + 4 * t * t * t * (1 - t) * y4 +
+               t * t * t * t * y5;
+               graphic.DrawLine(pen, x, y, x_, y_);
+               x = x_;
+               y = y_;
+            }
+
+        }
+        public void Draw(Graphics graphic, Pen pen)
+        {
+            
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (textBox3.Text == "") ||
+                (textBox4.Text == "") || (textBox5.Text == "") || (textBox6.Text == ""))
+                MessageBox.Show("Недостаточно точек для построения кривой Безье второго порядка", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                if ((textBox7.Text == "")||(textBox8.Text == ""))
+                    curve3(graphic, pen);
+                else
+                    if ((textBox9.Text == "") || (textBox10.Text == ""))
+                        curve4(graphic, pen);
+                    else
+                        curve5(graphic, pen);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics graphic = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Green);
+            
+            graphic.Clear(Color.Black);
+            Draw(graphic, pen);
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+            textBox9.Text = ""; 
+            textBox10.Text = "";
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
